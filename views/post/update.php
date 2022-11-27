784f1b0d34ae82d09ad17c84635836fccbcd0816
Anne-Catherine MICHAUD <71764180+annemhd@users.noreply.github.com>
2022-11-27 10:59:11 +0100

    update

--- views/post/update.php
+++ views/post/update.php
@@ -17,20 +17,26 @@
     $postModel = new Post('posts');
 
     $table = $postModel->selectPost($_POST['id']);
+
+    if (isset($_POST['updateB'])) {
+        $req = 'title=:title, content=:title';
+        $values = ['title' => $_POST['title'], 'content' => $_POST['content']];
+        $postModel->updatePost($_POST['id'], $req, $values);
+        echo 'Màj </br>';
+        echo '<a href="/">retour</a>';
+    } else {
+
     ?>
-    <form method="POST" action="/update">
-        <input type="hidden" name="id" value="<?= $table[0]->id ?>">
-        <input type="text" name="title" value="<?= $table[0]->title ?>">
-        <input type="text" name="content" value="<?= $table[0]->content ?>">
-        <input type="submit" name="update" value="Mettre à jour">
-    </form>
+
+        <form method="POST" action="">
+            <input type="hidden" name="id" value="<?= $table[0]->id ?>">
+            <input type="text" name="title" value="<?= $table[0]->title ?>">
+            <input type="text" name="content" value="<?= $table[0]->content ?>">
+            <input type="submit" name="updateB" value="Mettre à jour">
+        </form>
+
     <?php
 
-    if (isset($_POST['update'])) {
-        $req = 'title=:title, content=:title';
-        $test = ['title' => $_POST['title'], 'content' => $_POST['content']];
-        $postModel->updatePost($_POST['id'], $req, $test);
-        echo 'mise a jour éffectuée';
     }
 
     ?>
